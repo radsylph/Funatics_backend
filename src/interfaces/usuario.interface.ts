f32e2e94b88944fe5711eb9e428c70c1fc4d4e9f
radsylph <andresa@devtop.io>
2023-11-09 23:34:55 -0400

    feat: created more interfaces

--- src/interfaces/usuario.interface.ts
+++ src/interfaces/usuario.interface.ts
@@ -1,12 +1,13 @@
 export interface UsuarioInterface {
-    name: string;
-    lastname: string;
-    username: string;
-    email: string;
-    password: string;
-    token: string | null;
-    confirmado: boolean;
-    timestamps: boolean;
-    verificarPassword: (password: string) => Promise<boolean>;
-  }
-  
+  name: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+  profilePicture: string | null;
+  followers: number;
+  token: string | null;
+  confirmado: boolean;
+  timestamps: boolean;
+  verificarPassword: (password: string) => Promise<boolean>;
+}
